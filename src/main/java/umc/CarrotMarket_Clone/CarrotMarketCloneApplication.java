448017a5d44bc3874bf1441ded4f2f255b28dff6
mingsound21 <mingsound@kakao.com>
2022-11-12 03:32:56 +0900

    first commit

---
+++ src/main/java/umc/CarrotMarket_Clone/CarrotMarketCloneApplication.java
@@ -0,0 +1,13 @@
+package umc.CarrotMarket_Clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotMarketCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotMarketCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/CarrotMarket_Clone/CarrotMarketCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.CarrotMarket_Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotMarketCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
