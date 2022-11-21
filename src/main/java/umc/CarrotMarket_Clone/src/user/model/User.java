1b6a6b433ce4622c242994c25eeb5c524d1141d3
mingsound <62479120+mingsound21@users.noreply.github.com>
2022-11-22 00:01:42 +0900

    Comment: 주석 추가

--- src/main/java/umc/CarrotMarket_Clone/src/user/model/User.java
+++ src/main/java/umc/CarrotMarket_Clone/src/user/model/User.java
@@ -13,7 +13,6 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Table(name = "user")
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
